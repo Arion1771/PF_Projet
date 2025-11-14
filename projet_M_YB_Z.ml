793330e0321c07eb04a287ce42e20a3a3c5109b8
Matthieu Muller <matthieu.muller2@etu.univ-grenoble-alpes.fr>
2025-11-14 15:25:11 +0100

    debut typage

--- projet_M_YB_Z.ml
+++ projet_M_YB_Z.ml
@@ -1,1 +1,18 @@
 (*Exercice 1.1.1*)
+
+type Aexp =
+  | Aco of int
+  | Ava of int
+  | Amu of Aexp * Aexp
+  | Apl of Aexp * Aexp
+  | Amo of Aexp * Aexp
+
+type Bexp =
+  | Btrue of Bexp
+  | Bfalse of Bexp
+  | Bnot of Bexp -> Bexp
+  | Band of Bexp -> Bexp -> Bexp
+  | Bor of Bexp -> Bexp -> Bexp
+  | Beq of Bexp -> Bexp -> Bexp
+  | Beqnat of Aexp -> Aexp -> Bexp
+
