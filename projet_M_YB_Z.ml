2098ce95f2930f5b88a21ea885ec86d314baa1ce
Matthieu Muller <matthieu.muller2@etu.univ-grenoble-alpes.fr>
2025-12-04 19:00:46 +0100

    le taff de sami

--- projet_M_YB_Z.ml
+++ projet_M_YB_Z.ml
@@ -73,19 +73,50 @@
       | '1' -> Some (Aco 1)
       |  _  -> None)
 
-  let p_expr : (aexp, char) ranalist = 
-    p_var +| p_val
-
-  let p_skip : (instr,char) ranalist =
-    epsilon_res (Skip)
-
-  let p_assign : (instr, char) ranalist =
-    p_var ++> fun i ->
+  let rec p_final : (aexp, char) ranalist =
+    fun l -> 
+      ( (terminal '!' -+> p_final ++> fun f -> epsilon_res (Bnot f))
+      +| (p_expr ++> fun e -> epsilon_res (e))
+      +| (terminal '(' -+> p_disj ++> fun d -> terminal ')' -+> epsilon_res d)
+      ) l
+  and p_disj : (aexp, char) ranalist =
+    fun l ->
+      (p_conj ++> fun c1 ->
+      p_disj_s c1
+      ) l
+  and p_disj_s (acc : aexp) : (aexp, char) ranalist =
+    fun l ->
+      ( (terminal '+' -+> p_conj ++> fun c2 ->
+          let c = Bdisj (acc, c2) in
+          p_disj_s c)
+        +|
+        epsilon_res (acc)
+      ) l
+  and p_conj : (aexp, char) ranalist =
+    fun l ->
+      ( p_final ++> fun f1 ->
+        p_conj_s f1 
+      ) l
+  and p_conj_s (acc : aexp) : (aexp, char) ranalist =
+    fun l -> 
+      ( ( terminal '.' -+> p_final ++> fun f2 ->
+      let acc' = Bconj (acc, f2) in
+      p_conj_s acc')
+      +| epsilon_res (acc)
+      ) l
+  and p_expr : (aexp, char) ranalist = 
+    fun l -> 
+    ( p_var +| p_val
+    ) l
+  and p_assign : (instr, char) ranalist =
+    fun l ->
+    ( p_var ++> fun i ->
     terminal ':' -->
     terminal '=' -+>
-    (p_expr ++> fun e ->
-        epsilon_res (Assign(i, e)))
-
+    (p_expr ++> fun e -> epsilon_res (Assign(i, e)))
+    ) l
+  let p_skip : (instr,char) ranalist =
+    epsilon_res (Skip)
 
   let rec p_prog : (instr, char) ranalist =
     fun l ->
@@ -108,7 +139,7 @@
       ) l
   and p_if :(instr, char) ranalist =
     fun l ->
-      (terminal ('i') --> terminal ('(') -+> p_expr ++> fun cond -> terminal (')') -->
+      (terminal ('i') --> terminal ('(') -+> p_disj ++> fun cond -> terminal (')') -->
       terminal ('{') -+> p_prog ++> fun i1 -> terminal ('}') -->
       terminal ('{') -+> p_prog ++> fun i2 -> terminal ('}') -+>
       epsilon_res (If (cond, i1, i2))) l
@@ -116,12 +147,13 @@
     fun l ->
       ((terminal_res (fun c -> if c = 'w' then Some () else None)) ++>
       (fun _ -> (terminal_res (fun c -> if c = '(' then Some () else None)) ++>
-      (fun _ -> p_expr ++>  (fun cond -> (terminal_res (fun c -> if c = ')' then Some () else None)) ++>
+      (fun _ -> p_disj ++>  (fun cond -> (terminal_res (fun c -> if c = ')' then Some () else None)) ++>
       (fun _ -> (terminal_res (fun c -> if c = '{' then Some () else None)) ++>
       (fun _ -> p_prog ++>
       (fun body -> (terminal_res (fun c -> if c = '}' then Some () else None)) ++>
       (fun _ -> epsilon_res (While (cond, body)))))))))
     ) l
+  
 
   (*Exercice 2.1.2*)
 
@@ -129,11 +161,11 @@
     | Aco n -> "Aco " ^ string_of_int n
     | Ava n -> "Ava " ^ string_of_int n
     | Bnot e ->
-      "Bnot(" ^ string_of_aexp e ^ ")"
+      "Bnot ( " ^ string_of_aexp e ^ ")"
     | Bconj (e1, e2) ->
-      "Bconj(" ^ string_of_aexp e1 ^ ", " ^ string_of_aexp e2 ^ ")"
+      "Bconj ( " ^ string_of_aexp e1 ^ ", " ^ string_of_aexp e2 ^ ")"
     | Bdisj (e1, e2) ->
-      "Bdisj(" ^ string_of_aexp e1 ^ ", " ^ string_of_aexp e2 ^ ")"
+      "Bdisj ( " ^ string_of_aexp e1 ^ ", " ^ string_of_aexp e2 ^ ")"
 
   let rec string_of_instr = function
     | Skip ->
@@ -154,13 +186,14 @@
     let (ast, _) = p_prog (list_of_string s) in
     print_endline (string_of_instr ast)
 (*TESTS : Grammaire basique*)
-  let _ =show_ast "a:=0"
-  let _=  show_ast "a:=0;b:=1"
-  let _=  show_ast "w(a){a:=1}"
-  let _=  show_ast "i(a){a:=0}{b:=1}"
-  let _=  show_ast ";"
-  let _= show_ast "a:=1;b:=1;c:=1;w(a){i(c){c:=0;a:=b}{b:=0;c:=a}}"
-
+  let _ =show_ast "a:=0";;
+  let _=  show_ast "a:=0;b:=1";;
+  let _=  show_ast "w(a){a:=1}";;
+  let _=  show_ast "i(a){a:=0}{b:=1}";;
+  let _=  show_ast ";";;
+  let _= show_ast "a:=1;b:=1;c:=1;w(a){i(c){c:=0;a:=b}{b:=0;c:=a}}";;
+  let _= show_ast "i(a+b){a:=1}{b:=0}";;
+  ()
   (*TESTS : Expression avec disjonctions et conjonctions*)
 
   let test_aexp s =
